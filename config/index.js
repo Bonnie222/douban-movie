0863d2ea738ce829fd351b18349bd1022d693805
Bonnie222 <616247581@qq.com>
2017-10-11 10:42:48 +0800

    3.3

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',    //生产环境assetsPublicPath: '/'
-    assetsPublicPath: './',          //生产环境 staticPath:''
+    assetsPublicPath: '.',          //生产环境 staticPath:''
     // staticPath:'./',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
